2dbb7071757788027b40606e7461ad1459dffc0d
Islam Yamor <islamyamorz@gmail.com>
2025-07-07 20:28:39 +0300

    added json-server package

---
+++ packages/json-server/src/script.ts
@@ -0,0 +1,42 @@
+import { Executable } from '@clscripts/cl-common'
+
+export interface JsonServerOpts {
+  serveFrom: string
+  port: number
+  watch?: boolean
+}
+export class JsonServer implements Executable {
+  packageExecutable = 'json-server'
+  serveFrom: string
+  port: number
+  watch: boolean
+
+  constructor(opts: JsonServerOpts) {
+    this.serveFrom = opts.serveFrom
+    this.port = opts.port
+    this.watch = opts.watch ?? false
+  }
+
+  get command() {
+    return [
+      this.packageExecutable,
+      this.portArg,
+      this.watchArg,
+      this.serveFromArg, // <== JSON file path goes last
+    ]
+      .filter(Boolean)
+      .join(' ')
+  }
+
+  get portArg() {
+    return `--port ${this.port}`
+  }
+
+  get watchArg() {
+    return this.watch ? '--watch' : ''
+  }
+
+  get serveFromArg() {
+    return this.serveFrom
+  }
+}
