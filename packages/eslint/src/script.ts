19f7e3edf0e72e4fb09f6bcf493701f7a182b561
Islam Yamor <islamyamorz@gmail.com>
2025-07-07 20:26:12 +0300

    added eslint package

---
+++ packages/eslint/src/script.ts
@@ -0,0 +1,33 @@
+import { Executable } from '@clscripts/cl-common'
+
+export interface EslintOpts {
+  scanPath: string
+  fix?: boolean
+  ext?: string[]
+}
+export class Eslint implements Executable {
+  packageExecutable = 'eslint'
+  scanPath: string
+  fix?: boolean
+  ext?: string[]
+  constructor(opts: EslintOpts) {
+    this.scanPath = opts.scanPath
+    this.fix = opts.fix
+    this.ext = opts.ext
+  }
+  get command() {
+    return [this.packageExecutable, this.scanPathArg, this.extArgs, this.fixArg]
+      .filter(Boolean)
+      .join(' ')
+  }
+  get extArgs() {
+    if (!this.ext) return ''
+    else this.ext.map((ext) => `--ext ${ext}`).join(' ')
+  }
+  get fixArg() {
+    return this.fix ? '--fix' : ''
+  }
+  get scanPathArg() {
+    return this.scanPath
+  }
+}
