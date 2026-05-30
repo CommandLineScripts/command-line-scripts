da0febc9106a84aae9506a67be7a4b4578ba3c85
Islam Yamor <islamyamorz@gmail.com>
2026-05-30 07:31:59 +0300

    added noErrorOnUnmatchedPattern arg for prettier package

--- packages/prettier/src/script.ts
+++ packages/prettier/src/script.ts
@@ -3,22 +3,30 @@ import { Executable } from '@clscripts/cl-common'
 export interface PrettierOpts {
   files: string[]
   ignore?: string[]
+  noErrorOnUnmatchedPattern?: boolean
 }
 export class Prettier implements Executable {
   packageExecutable = 'prettier'
   files: string[]
   ignore?: string[]
+  noErrorOnUnmatchedPattern: boolean
   constructor(opts: PrettierOpts) {
     this.files = opts.files
     this.ignore = opts.ignore
+    this.noErrorOnUnmatchedPattern = opts.noErrorOnUnmatchedPattern ?? true
   }
   get command() {
-    return [this.packageExecutable, this.fileArg, this.ignoreArg].filter(Boolean).join(' ')
+    return [this.packageExecutable, this.fileArg, this.ignoreArg, this.noErrorOnUnmatchedPatternArg]
+      .filter(Boolean)
+      .join(' ')
   }
   get fileArg() {
     return `--write ${this.files.join(' ')}`
   }
   get ignoreArg() {
     return this.ignore ? `${this.ignore.map((path) => `!${path}`).join(' ')}` : ''
+  }
+  get noErrorOnUnmatchedPatternArg() {
+    return this.noErrorOnUnmatchedPattern ? `--no-error-on-unmatched-pattern` : ''
   }
 }
